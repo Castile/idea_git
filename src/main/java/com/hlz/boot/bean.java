acbcb8cebc6ce46d9c0707623b23159f3b6aae12
Castile <zhl396740445@163.com>
2021-05-07 21:59:52 +0800

    java bean目录

---
+++ src/main/java/com/hlz/boot/bean.java
@@ -0,0 +1,12 @@
+package com.hlz.boot;
+
+/**
+ * @author Hongliang Zhu
+ * @create 2021-05-07 21:58
+ */
+public class bean {
+    public static void main(String[] args) {
+
+        System.out.println("Hello");
+    }
+}
